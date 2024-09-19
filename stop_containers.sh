6d7622cb00a674efe29396b170439a47f35d001f
Sheheriyar99 <157871935+Sheheriyar99@users.noreply.github.com>
2024-09-19 19:09:38 +0500

    Update stop_containers.sh

--- stop_containers.sh
+++ stop_containers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ubuntu/node-todo-cicd
+cd /home/ubuntu/node-app
 docker ps --filter "expose=8000" -q | xargs -r docker stop
 docker ps -a --filter "expose=8000" -q | xargs -r docker rm
 docker-compose down
